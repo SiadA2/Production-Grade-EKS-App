84af5e0312ddc8fe7a368eace2536ed0f77e3eca
siadgit <siadadam2@gmail.com>
2025-11-25 15:54:47 +0000

    rewrite helmfile + change instance type in node grp

--- deployment/eks.tf
+++ deployment/eks.tf
@@ -34,7 +34,7 @@ module "eks" {
     example = {
       # Starting on 1.30, AL2023 is the default AMI type for EKS managed node groups
       ami_type       = "AL2023_x86_64_STANDARD"
-      instance_types = ["t3.micro"]
+      instance_types = ["m7i-flex.large"]
 
       min_size     = 3
       max_size     = 5
